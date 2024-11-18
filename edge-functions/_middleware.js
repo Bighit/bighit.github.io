d0285531875ae1c9800bf500c3432b48c7766bf7
Tianyu <393858338@qq.com>
2024-11-18 21:32:52 +0800

    Update _middleware.js

--- edge-functions/_middleware.js
+++ edge-functions/_middleware.js
@@ -5,10 +5,7 @@ export const config = {
 
 export default async (request, next) => {
   const url = new URL(request.url);
-
-  // 检查请求的路径是否为a.html
-  if (url.pathname === '/this_launches_in_popup.html') {
-    // 设置响应头以添加Cookie
+   // 设置响应头以添加Cookie
     const newHeaders = new Headers(request.headers);
     newHeaders.set('Set-Cookie', 'name=b; Path=/; Secure');
     const newRequest = new Request(request, {
@@ -17,8 +14,4 @@ export default async (request, next) => {
 
     // 继续处理请求，但使用新的请求头
     return next(newRequest);
-  }
-
-  // 对于其他请求，继续正常处理
-  return next();
 };
